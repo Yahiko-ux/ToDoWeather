18316a1e84b9b9678011dd83a3535f2ddbb3fe67
Buev1 <maks2550684@gmail.com>
2021-08-09 17:39:20 +0300

    feat: Add Room dependencies to project

--- app/src/main/java/com/todoweather/MainFragment.kt
+++ app/src/main/java/com/todoweather/MainFragment.kt
@@ -2,20 +2,16 @@ package com.todoweather
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.todoweather.databinding.FragmentMainBinding
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.fragment_main) {
+    private val binding: FragmentMainBinding by viewBinding()
 
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
     }
 
 }
